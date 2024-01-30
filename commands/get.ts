433ba2e1b05d969cbc969746c3e8ffad0b024d06
Bartek O <bartek@fos.nl>
2024-01-30 17:52:54 +0100

    Made get command parse given date
    with chrono so it supports "yesterday" like changed in the pipeline

--- commands/get.ts
+++ commands/get.ts
@@ -1,10 +1,11 @@
-import { command } from "cmd-ts";
+import { command, positional } from "cmd-ts";
+import DateType from "../misc/date_argument_type.ts";
 
 
 const getDate = command({
     name: "get",
     args: {
-
+        date: positional({ type: DateType, displayName: "date", description: "for what date to get songs" })
     },
     handler: args => {
         console.log(`getting date`, args);
---
+++ misc/date_argument_type.ts
@@ -0,0 +1,18 @@
+import { Type } from "cmd-ts";
+import { parseDate } from "chrono";
+
+const Date: Type<string, Date> = {
+    displayName: "date time",
+    description: "parses given date string to date using Chrono (so 'yesterday' is supported)",
+    from: async input => {
+        const result = parseDate(input);
+
+        if (result === null) {
+            throw `failed to parse date '${input}'`;
+        }
+
+        return result;
+    }
+}
+
+export default Date;
