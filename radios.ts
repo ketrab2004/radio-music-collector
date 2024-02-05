71c849d17f5ee7f3df9160fcc0ed16a064b75241
Bartek O <bartek@fos.nl>
2024-02-05 22:07:21 +0100

    Commented out rs_ids of radio stations
    they seem to make the requests return empty arrays...

--- radios.ts
+++ radios.ts
@@ -20,31 +20,31 @@ export const radios: Radio[] = [
         name: "Sublime Soul",
         domain: "https://myonlineradio.nl/",
         rid: 25,
-        rs_id: 8697260,
+        // rs_id: 8697260,
         hash: "5e5604312dfc81967b7c5ad29604ca76"
     }, {
         name: "Sublime Smooth",
         domain: "https://myonlineradio.nl/",
         rid: 25,
-        rs_id: 8678180,
+        // rs_id: 8678180,
         hash: "e42b204798a21435aa589a54c58dfc2b"
     }, {
         name: "Sublime Classics",
         domain: "https://myonlineradio.nl/",
         rid: 25,
-        rs_id: 8699254,
+        // rs_id: 8699254,
         hash: "d9b0e41eb4c65a98329fae1ccda1fc8e"
     }, {
         name: "Sublime Funk & Disco",
         domain: "https://myonlineradio.nl/",
         rid: 25,
-        rs_id: 8699310,
+        // rs_id: 8699310,
         hash: "1e050079ad4db07f50e1468492ff8d2a"
     }, {
         name: "Sublime Jazz",
         domain: "https://myonlineradio.nl/",
         rid: 25,
-        rs_id: 8698771,
+        // rs_id: 8698771,
         hash: "03d588b4a8b8f12ba2f3614d3d52355f"
     },
     {
